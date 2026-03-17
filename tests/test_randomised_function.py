4bdbb518fed69b4be73f0a327351ba8ceeeaf202
Dovydas1118 <dovydasvolkovas@gmail.com>
2026-03-17 14:04:20 +0000

    randomised function passes all tests

--- tests/test_randomised_function.py
+++ tests/test_randomised_function.py
@@ -1,8 +1,15 @@
 import unittest
+from unittest.mock import patch
 from lecture import randomised_function
 
 class MyTestCase(unittest.TestCase):
 
-    def test_randomised_function(self):
-        self.assertEqual('software', randomised_function())  # This will pass or fail randomly
-        # TODO: Can we make this test deterministic? (HINT: Mock testing)
+    @patch("lecture.randomised_function")
+    def test_randomised_function_with_mock1(self, randomised_function_mock):
+        randomised_function_mock.return_value = 'software'
+        self.assertEqual(randomised_function_mock(), 'software')
+
+    @patch("lecture.randomised_function")
+    def test_randomised_function_with_mock2(self, randomised_function_mock):
+        randomised_function_mock.return_value = 'engineering'
+        self.assertEqual(randomised_function_mock(), 'engineering')
